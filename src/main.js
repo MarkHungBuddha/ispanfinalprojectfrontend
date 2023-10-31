a14589255120d35a33e601da7366242ce91fb465
pk6965050 <142571927+pk6965050@users.noreply.github.com>
2023-10-31 11:14:37 +0800

    更新
    1.login .vue
    2.app.vue
    3.main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
-import { createApp } from 'vue'
-import './assets/main.css'
-import 'vuetify/styles'
-import{ createVuetify } from 'vuetify';
-// import Vuetify from 'vuetify';
+import { createApp } from 'vue';
+import axios from 'axios';  // 引入 Axios
+import './assets/main.css';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
 
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-// import 'bootstrap-icons/font/bootstrap-icons.css'
-// import '@/assets/bootstrap.min.css'
-// import '@/assets/bootstrap.min.js'
-import router from './router'
-import App from './App.vue'
+import router from './router';
+import App from './App.vue';
 import sidebar from "@/components/sidebar.vue";
 import Home from "@/views/Home.vue";
 import productPage from "@/views/productPage.vue";
 
+// 設定 Axios
+axios.defaults.withCredentials = true;
 
-const app = createApp(App)
+const app = createApp(App);
 
 const vuetify = createVuetify({
     theme: {
@@ -25,11 +24,13 @@ const vuetify = createVuetify({
     },
     components,
     directives,
-})
+});
 
 app.component('sidebar', sidebar);
 app.component('Home', Home);
 app.component('productPage', productPage);
 
-app.use(vuetify).use(router).mount('#app');
+// （可選）將 Axios 設定為全局屬性，這樣您可以在任何組件中使用 this.$axios 進行請求
+app.config.globalProperties.$axios = axios;
 
+app.use(vuetify).use(router).mount('#app');
