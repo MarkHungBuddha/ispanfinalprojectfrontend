102b1ab92fa1ddf571d437a7dfdabf3559656956
pk6965050 <pk6965050@gmail.com>
2023-11-01 00:38:45 +0800

    categoryname.vue
    categoryname.vue
    功能有問題˙還要測試

---
+++ src/components/categoryname.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="text-center">
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+                <v-btn color="primary" v-bind="props"> 行動通訊 </v-btn>
+            </template>
+            <v-list>
+                <v-list-item v-for="(item, index) in items" :key="index">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ on }">
+                <v-btn color="primary" v-on="on">大型家電</v-btn>
+            </template>
+            <v-list>
+                <v-list-item v-for="(item, index) in items2" :key="index">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </div>
+</template>
+  
+<script>
+export default {
+    data: () => ({
+        items: [
+            { title: '行動電源_手機配件' },
+            { title: '相機配件' },
+            { title: '行車導航' },
+            { title: '手機' },
+            { title: '相機_攝影' },
+            { title: '穿戴裝置' },
+            { title: '平板電腦' },
+        ],
+        items2: [
+            { title: '洗衣機' },
+            { title: '乾衣機' },
+        ],
+    }),
+}
+</script>
+    
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,24 +4,25 @@ import { createApp } from 'vue';
 import Slider from "@/components/slider.vue";
 import RegisterForm from "@/components/RegisterForm.vue";
 import navbar from "@/components/navbar.vue";
+import categoryname from '../components/categoryname.vue';
 </script>
 
 <template>
-
   <v-app>
     <v-row justify="center">
       <v-col>
-        <navbar/>
-        <slider class="custom-slider"/>
+        <navbar />
+        <categoryname></categoryname>
+        <slider class="custom-slider" />
       </v-col>
     </v-row>
   </v-app>
 </template>
 
 <style scoped>
-
 .custom-slider {
   max-width: 80vw;
-  margin: 0 auto;  /* 置中容器 */
+  margin: 0 auto;
+  /* 置中容器 */
 }
 </style>
